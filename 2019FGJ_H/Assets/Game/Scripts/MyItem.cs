195d1fd098f57e1539d02475def37483f333ba33
Rempty <emptygamer5566@gmail.com>
2019-08-24 18:11:40 +0800

    stone prefab3

---
+++ 2019FGJ_H/Assets/Game/Scripts/MyItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyItem : MonoBehaviour, INotification
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NotificationCenter.Default.AddObserver(this,"OH");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnNotify(Notification eventdata){
+
+    }
+}
