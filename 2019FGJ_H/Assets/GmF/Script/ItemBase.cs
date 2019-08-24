e96ec5e2be01857054dc7ea44c981207642d2781
gmfnasg12 <gpx_mark_farng@hotmail.com>
2019-08-24 20:33:09 +0800

    AddCreateItemFuction

--- 2019FGJ_H/Assets/GmF/Script/ItemBase.cs
+++ 2019FGJ_H/Assets/GmF/Script/ItemBase.cs
@@ -6,12 +6,55 @@ using UnityEngine;
 [RequireComponent(typeof(ModifyValueBase))]
 public class ItemBase : TriggerBase
 {
+    public System.Action<ItemBase> OnDeath = null;
     public ModifyValueBase modifyData;
+    public float LifeTime = 5;
+    private float overLifeTime = 0;
 
     private void Awake()
     {
         modifyData = gameObject.GetComponent<ModifyValueBase>();
         SendData = modifyData.ModifyVlue;
         KeyName = modifyData.ModifyKeyString;
+
+        if(ItemManager.Instance != null)
+        {
+            if (ItemManager.Instance != null && !ItemManager.Instance.SampleItems.Contains(this))
+            {
+                ItemManager.Instance.aliveItems.Add(this);
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+       
+    }
+
+    void Update()
+    {
+        CheckLife();
+        overLifeTime += Time.deltaTime;
+    }
+
+    void CheckLife()
+    {
+        if (ItemManager.Instance != null && ItemManager.Instance.SampleItems.Contains(this))
+        {
+            return;
+        }
+        if (overLifeTime > LifeTime)
+        {
+            if (ItemManager.Instance != null)
+            {
+                ItemManager.Instance.aliveItems.Remove(this);
+            }
+            Destroy(gameObject);
+
+            if(OnDeath != null)
+            {
+                OnDeath.Invoke(this);
+            }
+        }
     }
 }
---
+++ 2019FGJ_H/Assets/GmF/Script/ItemManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public static ItemManager Instance;
+
+    public List<ItemBase> SampleItems = new List<ItemBase>();
+
+    public List<ItemBase> aliveItems = new List<ItemBase>();
+
+    public List<ItemPoint> ItemPoints = new List<ItemPoint>();
+    ItemPoint lastPutItemPoint = null;
+
+    public int MaxItemCount = 2;
+    public float CreateItemDurateTime = 10;
+    float lastCreateItmTime = 0;
+    public bool FirstCeateItem;
+
+    void Awake()
+    {
+        Instance = this;
+        for (int i = 0; i < SampleItems.Count; i++)
+        {
+            SampleItems[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        RandomCreateItem();
+    }
+
+    void RandomCreateItem()
+    {
+        if (SampleItems.Count == 0)
+        {
+            return;
+        }
+
+        if(aliveItems.Count >= MaxItemCount)
+        {
+            return;
+        }
+
+        if (lastCreateItmTime == 0 && FirstCeateItem)
+        {
+            CreateItem();
+        }
+
+        if (Time.time - lastCreateItmTime > CreateItemDurateTime)
+        {
+            CreateItem();
+        }
+    }
+
+    void CreateItem()
+    {
+        List<ItemPoint> tmpItemPoint = ItemPoints.FindAll(e => !e.IsHaveItem);
+        if(tmpItemPoint.Count == 0)
+        {
+            return;
+        }
+
+        ItemPoint newPoint = tmpItemPoint[Random.Range(0, tmpItemPoint.Count)];
+
+        ItemBase sampleItem = SampleItems[Random.Range(0, SampleItems.Count)];
+        newPoint.CreateItem(sampleItem);
+    }
+
+    //Vector3 RandomCreateItemPos()
+    //{
+    //    if(ItemPoints.Count == 0)
+    //    {
+    //        return Vector3.zero;
+    //    }
+
+    //    if(ItemPoints.Count == 1)
+    //    {
+    //        lastPutItemPoint = ItemPoints[0];
+    //        return lastPutItemPoint.transform.position;
+    //    }
+
+    //    List<ItemPoint> tmpItemPoints = ItemPoints.FindAll(e => e != lastPutItemPoint);
+
+    //    lastPutItemPoint = tmpItemPoints[Random.Range(0, tmpItemPoints.Count)];
+
+    //    return lastPutItemPoint.transform.position;
+    //}
+}
