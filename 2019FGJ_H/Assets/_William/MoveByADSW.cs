4422dc4aef8f964e8fb1fc1f6a0f0980628a698d
vvbest2012 <vvbest2012@gmail.com>
2019-08-24 14:49:25 +0800

    update asset 2D Platformer Controller. update Map image test use by edge collider 2d. update hook behavior. reference by https://blog.csdn.net/MacYosef/article/details/77620675, modified to become 2d version

---
+++ 2019FGJ_H/Assets/_William/MoveByADSW.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveByADSW : MonoBehaviour
+{
+    //人物状态
+    public const int HERO_UP = 0;
+    public const int HERO_RIGHT = 1;
+    public const int HERO_DOWN = 2;
+    public const int HERO_LEFT = 3;
+
+    //人物当前行走的方向状态  
+    public int state = 0;
+    //人物移动速度  
+    public int moveSpeed = 2;
+
+    //初始化人物位置  
+    public void Awake()
+    {
+        state = HERO_UP;
+    }
+    // Use this for initialization  
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        //获取控制的方向， 上下左右，   
+        float KeyVertical = Input.GetAxis("Vertical");
+        float KeyHorizontal = Input.GetAxis("Horizontal");
+        //Debug.Log("keyVertical" + KeyVertical);
+        //Debug.Log("keyHorizontal" + KeyHorizontal);
+        if (KeyVertical == -1)
+        {
+            setHeroState(HERO_DOWN); //下 
+        }
+        else if (KeyVertical == 1)
+        {
+            setHeroState(HERO_UP);  //上
+        }
+        if (KeyHorizontal == 1)
+        {
+            setHeroState(HERO_RIGHT); //右 
+        }
+        else if (KeyHorizontal == -1)
+        {
+            setHeroState(HERO_LEFT);  //左
+        }
+
+        //if (KeyVertical == 0 && KeyHorizontal == 0)
+        //{
+        //    animation.Play("idle");
+        //}
+    }
+
+    void setHeroState(int newState)
+    {
+        //根据当前人物方向与上一次备份的方向计算出模型旋转的角度  
+        int rotateValue = (newState - state) * 90;
+        Vector3 transformValue = new Vector3();
+
+        //播放行走动画  
+        //animation.Play("walk");
+
+        //模型移动的位置数值  
+        switch (newState)
+        {
+            case HERO_UP:
+                transformValue = Vector3.up * Time.deltaTime;
+                break;
+            case HERO_DOWN:
+                transformValue = (-Vector3.up) * Time.deltaTime;
+                break;
+            case HERO_LEFT:
+                transformValue = Vector3.left * Time.deltaTime;
+                break;
+            case HERO_RIGHT:
+                transformValue = (-Vector3.left) * Time.deltaTime;
+                break;
+        }
+
+        //transform.Rotate(Vector3.up, rotateValue);
+        //移动人物  
+        transform.Translate(transformValue * moveSpeed, Space.World);
+        state = newState;
+    }
+}
---
+++ 2019FGJ_H/Assets/_William/Player.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace William
+{
+    public class Player : MonoBehaviour
+    {
+        public LineRenderer hookObj;                //钩子起始点 附加有LineRenderer组件
+        public float hookSpeed = 10f;               //钩子发射速度
+        public float totalDistance = 3f;            //钩子最大长度
+        public bool isOut = false;                  //钩子发射或者收回标记
+        public Transform hookTransform;             //钩子物体（即小球）
+
+        private Collider[] colCollection;           //发射中碰撞到的物体
+        private float oriZvalue;
+        private bool isStop = true;                 //用来判断钩子是否停止
+        private float tempZvalue;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                isOut = true;
+            }
+            
+            checkCollider();
+            CheckLength();
+
+            //发射钩子
+            if (isOut)
+            {
+                //Debug.Log("test");
+                hookTransform.Translate(0, hookSpeed * Time.deltaTime, 0);
+                hookObj.SetPosition(1, hookTransform.localPosition);
+            }
+            //收回钩子
+            else
+            {
+                if (hookTransform.localPosition.y > oriZvalue)
+                {
+                    hookTransform.Translate(0, -hookSpeed * Time.deltaTime,0);
+                    hookObj.SetPosition(1, hookTransform.localPosition);
+                }
+            }
+
+            //判断钩子是否为停止状态
+            if (hookTransform.childCount > 0)
+            {
+                isStop = Mathf.Abs(hookTransform.localPosition.z - tempZvalue) <= 0 ? true : false;
+            }
+            else
+                isStop = false;
+            //将敌人够到身前后，放开钩子
+            if (isStop) RealeaseChild();
+        }
+
+        void checkCollider()
+        {
+            //对钩子进行球形检测，返回所有碰到或者在球范围内的碰撞体数组     
+            //注意将人称控制器及其子物体的Layer修改为不是Default的一个层，否则钩子会检测到自身的碰撞体
+            colCollection = Physics.OverlapSphere(hookTransform.position, 0.2f, 1 << LayerMask.NameToLayer("Default"));
+            if (colCollection.Length > 0)
+            {
+                foreach (Collider item in colCollection)
+                {
+                    //将敌人的tag设置为“Enemy”
+                    if (item.gameObject.tag.Equals("Enemy"))
+                        item.transform.SetParent(hookTransform);
+                }
+                isOut = false;
+            }
+        }
+
+        void CheckLength()
+        {
+            Debug.Log("Distance" + Vector3.Distance(hookTransform.position, hookObj.transform.position));
+            if (Vector3.Distance(hookTransform.position, hookObj.transform.position) > totalDistance)
+            {
+                isOut = false;
+            }
+        }
+
+        void RealeaseChild()
+        {
+            if (hookTransform.childCount > 0)
+            {
+                for (int i = 0; i < hookTransform.childCount; i++)
+                {
+                    hookTransform.GetChild(i).transform.SetParent(null);
+                }
+            }
+        }
+
+    }
+}
