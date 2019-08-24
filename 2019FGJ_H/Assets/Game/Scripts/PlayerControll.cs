adcb93b1803ff19db5c5c370e25502bc8b33fb26
licky0504 <zxc6338983@gmail.com>
2019-08-24 20:44:57 +0800

    animaiton setting

--- 2019FGJ_H/Assets/Game/Scripts/PlayerControll.cs
+++ 2019FGJ_H/Assets/Game/Scripts/PlayerControll.cs
@@ -22,10 +22,11 @@ public class PlayerControll : MonoBehaviour
     public delegate void PlayerShoot();
     public PlayerShoot m_PlayerShoot;
 
+    private Animator tarAni;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tarAni = targetObj.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,16 +35,37 @@ public class PlayerControll : MonoBehaviour
         switch (_MoveState)
         {
             case MoveState.idle:
+                tarAni.Play("Idle");
                 break;
             case MoveState.up:
                 transform.position += moveSpeed*transform.up * Time.fixedDeltaTime;
                 if (targetObj)
+                {
                     targetObj.transform.position = transform.position;
+                    if (transform.rotation.eulerAngles.z >= 0&& transform.rotation.eulerAngles.z < 180)
+                    {
+                        tarAni.Play("run1");
+                    }
+                    else if(transform.rotation.eulerAngles.z >=180 && transform.rotation.eulerAngles.z < 360)
+                    {
+                        tarAni.Play("run2");
+                    }
+                }
                 break;
             case MoveState.down:
                 transform.localPosition -= moveSpeed*transform.up * Time.fixedDeltaTime;
-                if(targetObj)
+                if (targetObj)
+                {
                     targetObj.transform.position = transform.position;
+                    if (transform.rotation.z > 0 && transform.rotation.z <= 180)
+                    {
+                        tarAni.Play("run1");
+                    }
+                    else if (transform.rotation.z <= 0 && transform.rotation.z > -180)
+                    {
+                        tarAni.Play("run2");
+                    }
+                }
                 break;
             default:
                 break;
